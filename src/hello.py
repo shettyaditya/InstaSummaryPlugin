c08df011858d0ecf7dcaec109601f0277e3b3c04
Aditya Shetty <shettyaditya9879@gmail.com>
2017-09-30 11:48:41 +0530

    InstaSummary Extension/Plugin
    A chrome extension which is used to summarize the current page.

---
+++ src/hello.py
@@ -0,0 +1,175 @@
+# coding=UTF-8
+from __future__ import division
+import re
+
+# This is a naive text summarization algorithm
+# Created by Shlomi Babluki
+# April, 2013
+
+
+class SummaryTool(object):
+
+    # Naive method for splitting a text into sentences
+    def split_content_to_sentences(self, content):
+        content = content.replace("\n", ". ")
+        return content.split(". ")
+
+    # Naive method for splitting a text into paragraphs
+    def split_content_to_paragraphs(self, content):
+        return content.split("\n\n")
+
+    # Caculate the intersection between 2 sentences
+    def sentences_intersection(self, sent1, sent2):
+
+        # split the sentence into words/tokens
+        s1 = set(sent1.split(" "))
+        s2 = set(sent2.split(" "))
+
+        # If there is not intersection, just return 0
+        if (len(s1) + len(s2)) == 0:
+            return 0
+
+        # We normalize the result by the average number of words
+        return len(s1.intersection(s2)) / ((len(s1) + len(s2)) / 2)
+
+    # Format a sentence - remove all non-alphbetic chars from the sentence
+    # We'll use the formatted sentence as a key in our sentences dictionary
+    def format_sentence(self, sentence):
+        sentence = re.sub(r'\W+', '', sentence)
+        return sentence
+
+    # Convert the content into a dictionary <K, V>
+    # k = The formatted sentence
+    # V = The rank of the sentence
+    def get_senteces_ranks(self, content):
+
+        # Split the content into sentences
+        sentences = self.split_content_to_sentences(content)
+
+        # Calculate the intersection of every two sentences
+        n = len(sentences)
+        values = [[0 for x in xrange(n)] for x in xrange(n)]
+        for i in range(0, n):
+            for j in range(0, n):
+                values[i][j] = self.sentences_intersection(sentences[i], sentences[j])
+
+        # Build the sentences dictionary
+        # The score of a sentences is the sum of all its intersection
+        sentences_dic = {}
+        for i in range(0, n):
+            score = 0
+            for j in range(0, n):
+                if i == j:
+                    continue
+                score += values[i][j]
+            sentences_dic[self.format_sentence(sentences[i])] = score
+        return sentences_dic
+
+    # Return the best sentence in a paragraph
+    def get_best_sentence(self, paragraph, sentences_dic):
+
+        # Split the paragraph into sentences
+        sentences = self.split_content_to_sentences(paragraph)
+
+        # Ignore short paragraphs
+        if len(sentences) < 2:
+            return ""
+
+        # Get the best sentence according to the sentences dictionary
+        best_sentence = ""
+        max_value = 0
+        for s in sentences:
+            strip_s = self.format_sentence(s)
+            if strip_s:
+                if sentences_dic[strip_s] > max_value:
+                    max_value = sentences_dic[strip_s]
+                    best_sentence = s
+
+        return best_sentence
+
+    # Build the summary
+    def get_summary(self, title, content, sentences_dic):
+
+        # Split the content into paragraphs
+        paragraphs = self.split_content_to_paragraphs(content)
+
+        # Add the title
+        summary = []
+        summary.append(title.strip())
+        summary.append("")
+
+        # Add the best sentence from each paragraph
+        for p in paragraphs:
+            sentence = self.get_best_sentence(p, sentences_dic).strip()
+            if sentence:
+                summary.append(sentence)
+
+        return ("\n").join(summary)
+
+
+# Main method, just run "python summary_tool.py"
+def main():
+
+    # Demo
+    # Content from: "http://thenextweb.com/apps/2013/03/21/swayy-discover-curate-content/"
+
+    title = """
+    Swayy is a beautiful new dashboard for discovering and curating online content [Invites]
+    """
+
+    content = """
+    Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented,[14] and specifically designed to have as few implementation dependencies as possible. It is intended to let application developers "write once, run anywhere" (WORA),[15] meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.[16] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of computer architecture. As of 2016, Java is one of the most popular programming languages in use,[17][18][19][20] particularly for client-server web applications, with a reported 9 million developers.[21] Java was originally developed by James Gosling at Sun Microsystems (which has since been acquired by Oracle Corporation) and released in 1995 as a core component of Sun Microsystems' Java platform. The language derives much of its syntax from C and C++, but it has fewer low-level facilities than either of them.
+
+The original and reference implementation Java compilers, virtual machines, and class libraries were originally released by Sun under proprietary licenses. As of May 2007, in compliance with the specifications of the Java Community Process, Sun relicensed most of its Java technologies under the GNU General Public License. Others have also developed alternative implementations of these Sun technologies, such as the GNU Compiler for Java (bytecode compiler), GNU Classpath (standard libraries), and IcedTea-Web (browser plugin for applets).
+
+The latest version is Java 9, released on September 21, 2017,[22] and is one of the two versions currently supported for free by Oracle. Versions earlier than Java 8 are supported both by Oracle and other companies on a commercial basis.
+
+There were five primary goals in the creation of the Java language:[16]
+
+It must be "simple, object-oriented, and familiar".
+It must be "robust and secure".
+It must be "architecture-neutral and portable".
+It must execute with "high performance".
+It must be "interpreted, threaded, and dynamic".
+
+
+
+Versions
+Main article: Java version history
+As of 2017, both Java 8 and 9 are officially supported. Major release versions of Java, along with their release dates:
+
+JDK 1.0 (January 23, 1996)[39]
+JDK 1.1 (February 19, 1997)
+J2SE 1.2 (December 8, 1998)
+J2SE 1.3 (May 8, 2000)
+J2SE 1.4 (February 6, 2002)
+J2SE 5.0 (September 30, 2004)
+Java SE 6 (December 11, 2006)
+Java SE 7 (July 28, 2011)
+Java SE 8 (March 18, 2014)
+Java SE 9 (September 21, 2017
+
+
+    """
+
+    # Create a SummaryTool object
+    st = SummaryTool()
+
+    # Build the sentences dictionary
+    sentences_dic = st.get_senteces_ranks(content)
+
+    # Build the summary with the sentences dictionary
+    summary = st.get_summary(title, content, sentences_dic)
+
+    # Print the summary
+    print summary
+
+    # Print the ratio between the summary length and the original length
+    print ""
+    print "Original Length %s" % (len(title) + len(content))
+    print "Summary Length %s" % len(summary)
+    print "Summary Ratio: %s" % (100 - (100 * (len(summary) / (len(title) + len(content)))))
+
+
+if __name__ == '__main__':
+    main()
